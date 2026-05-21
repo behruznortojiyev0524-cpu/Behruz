913f4ad4b941cb8b87de1bdb8087f7a3e8b06625
Behruz <akmalsayfullayev19@gmail.com>
2026-05-22 00:16:03 +0500

    Updates

--- src/components/ui/SlideLayout.jsx
+++
@@ -1,7 +0,0 @@
-export default function SlideLayout({ children, className = '' }) {
-  return (
-    <div className={`w-full h-full flex flex-col bg-white text-slate-800 select-none overflow-hidden ${className}`}>
-      {children}
-    </div>
-  )
-}
